2ccadfcd0c255dce2d665ca2577aff931357d243
hossain-shuvo7860 <shahadat@baiust.edu.bd>
2021-06-14 00:51:31 +0600

    updated

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -25,3 +25,28 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# rom django.contrib import admin
+# from django.urls import path, include
+# from django.conf.urls import url
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from django.views.static import serve as mediaserve
+#
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('blog.urls', namespace='blog')),
+#     path('summernote/', include('django_summernote.urls'))
+#
+# ]
+#
+# urlpatterns.append(url(f'^{settings.MEDIA_URL.lstrip("/")}(?P<path>.*)$',
+#                      mediaserve, {'document_root': settings.MEDIA_ROOT}))
+#
+# urlpatterns += staticfiles_urlpatterns()
+#
+#
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,
+#     document_root=settings.MEDIA_ROOT)
